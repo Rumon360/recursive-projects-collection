360ca08b4212d309531ac2e7e379da3a7d4b52e4
Rumon360 <hmk.rumon@gmail.com>
2025-11-28 16:01:42 +0600

    feat: Introduce kanban board page with new components and add lucide-react dependency.

--- src/pages/kanban-board/components/card.tsx
+++ src/pages/kanban-board/components/card.tsx
@@ -1,21 +1,5 @@
-import type { CardType } from "../types";
-
-interface CardProps {
-  task: CardType;
-}
-
-function Card({ task }: CardProps) {
-  return (
-    <div className="shadow border border-zinc-700 rounded-lg p-4 bg-zinc-800/60">
-      <div>
-        <h2 className="text-lg lg:text-xl font-semibold">{task.title}</h2>
-        <div className="pt-4 flex justify-between items-center w-full text-neutral-400">
-          <p className="text-sm lg:text-base">{task.id}</p>
-          <p className="text-sm lg:text-base">{task.points}</p>
-        </div>
-      </div>
-    </div>
-  );
+function Card() {
+  return <></>;
 }
 
 export default Card;
