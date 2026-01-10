3e017a6108fe007c71a9a87e5a0c7adae39248ba
Rumon360 <hmk.rumon@gmail.com>
2026-01-10 19:43:54 +0600

    chore: note shell ui

--- src/pages/note-shell/index.tsx
+++ src/pages/note-shell/index.tsx
@@ -1,5 +1,24 @@
+import { useState, type FormEvent } from "react";
+import ShellInput from "./components/input";
+
 function NoteShell() {
-  return <div className="font-terminal"></div>;
+  const [value, setValue] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (value.trim() === "") return;
+    setValue("");
+  };
+
+  return (
+    <div className="h-screen w-full font-terminal overflow-hidden">
+      <ShellInput
+        value={value}
+        setValue={setValue}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
 }
 
 export default NoteShell;
